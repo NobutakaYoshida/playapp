2cc5c124108ed06dbcad5ab05aa2831b8e1e29e8
yoshidanobutaka <nobutaka.yoshida@rakus-partners.co.jp>
2022-01-05 16:48:58 +0900

    2.1.1 bug fix

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -16,20 +16,9 @@ public class Application extends Controller {
     }
 
     // ルートにアクセスした際のAction
-    public static Result index() {
-        return ok(index.render("何か書いて。", new Form(SampleForm.class)));
+    public Result index() {
+        return ok(index.render("何か書いて。"));
     }
 
-    // /sendにアクセスした際のAction
-    public static Result send() {
-        Form<SampleForm> f = form(SampleForm.class).bindFromRequest();
-        if (!f.hasErrors()) {
-            SampleForm data = f.get();
-            String msg = "you typed: " + data.message;
-            return ok(index.render(msg, f));
-        } else {
-            return badRequest(index.render("ERROR", form(SampleForm.class)));
-        }
-    }
 
 }
